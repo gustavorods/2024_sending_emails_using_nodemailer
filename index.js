e2548b990d4c95ade933a4a429dafafc5f0613ef
gustavorods <gustavo.rlsilva07@gmail.com>
2024-08-24 20:58:18 -0300

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+// importing .env
+require('dotenv').config();
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: true, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Testing" <Testing@example.com>', // sender address
+    to: process.env.RECIPIENT, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
